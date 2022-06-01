a8fc06b5e0da0e5c87760b4cee098fc404b12071
Vertic <thisistherealequinoctic@gmail.com>
2022-06-01 17:57:30 +0800

    Update MouseRaycastingTesting.lua

--- Tests/MouseRaycastingTesting.lua
+++ Tests/MouseRaycastingTesting.lua
@@ -1,7 +1,16 @@
 local UserInputService = game:GetService("UserInputService")
-
-while true do
+local RunService = game:GetService("RunService")
+local camera = workspace.CurrentCamera
+local function MouseRaycast()
 	local mousePos = UserInputService:GetMouseLocation()
-	task.wait(3)
-	print(mousePos)
+	local mouseRay = camera:ViewportPointToRay(mousePos.X, mousePos.Y)
+	local raycastResult = workspace:Raycast(mouseRay.Origin, mouseRay.Direction * 1000)
+	return raycastResult
 end
+
+RunService.RenderStepped:Connect(function()
+	local result = MouseRaycast()
+	if result and result.Instance then
+		print(result.Instance.Name)
+	end
+end)
