6ae4f703241f528eaf7792d32717866f91be4069
Vertic <thisistherealequinoctic@gmail.com>
2022-06-01 18:31:42 +0800

    Add in script templates

---
+++ Templates/SampleModuleScript.lua
@@ -0,0 +1,30 @@
+--[[
+	This template can be used for a ModuleScript(s)
+]]
+
+
+-- Main Module Variable (DO NOT REMOVE)
+
+local module = {}
+
+
+-- Services - Implement some of the game's available services here
+
+local service = game:GetService("Service")
+
+
+-- Objects - Add in some variables here
+
+local var = value
+
+
+-- Module Functions - Implement some functions for the module
+
+function module.Function(params)
+	-- Code block
+end
+
+
+-- Return the module (DO NOT REMOVE THIS AS WELL)
+
+return module
