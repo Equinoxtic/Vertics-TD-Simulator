9d597f3681ee975c41dea38d6748096d62428c3f
Vertic <thisistherealequinoctic@gmail.com>
2022-06-01 17:33:46 +0800

    Update TowerTesting.lua

--- Tests/TowerTesting.lua
+++ Tests/TowerTesting.lua
@@ -1,11 +1,25 @@
 local tower = script.Parent
 local mobs = workspace.Mobs
 
-local function GetEnemyPosition()
+local function FindNearestTarget()
+	local maxDistance = 50
+	local nearestTarget = nil
 	for i, target in ipairs(mobs:GetChildren()) do
 		local distance = (target.HumanoidRootPart.Position - tower.Position).Magnitude
 		print(target.Name, distance)
+		if distance < maxDistance then
+			print(target.Name, " is the nearest target")
+			nearestTarget = target
+			maxDistance = distance
+		end
 	end
+	return nearestTarget
 end
 
-mobs.ChildAdded:Connect(GetEnemyPosition)
+while true do
+	local target = FindNearestTarget()
+	if target then
+		target.Humanoid:TakeDamage(2)
+	end
+	task.wait(1)
+end
