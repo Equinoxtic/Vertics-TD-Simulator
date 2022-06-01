30d2ae594703e725785931387b23b44c36dedd86
Vertic <thisistherealequinoctic@gmail.com>
2022-06-02 01:43:29 +0800

    Update NewAnimationHandler.lua

---
+++ Tests/NewAnimationHandler.lua
@@ -0,0 +1,29 @@
+local function setAnimation(object, animName)
+	local humanoid = object:WaitForChild("Humanoid")
+	if humanoid then
+		local animObj = humanoid:WaitForChild(animName)
+		if animObj then
+			local animator = humanoid:FindFirstChild("Animator") or Instance.new("Animator", humanoid)
+			local animTrack = animator:LoadAnimation(animObj)
+			return animTrack
+		end
+	end
+end
+
+local function playAnimation(object, animName)
+	local animTrack = setAnimation(object, animName)
+	if animTrack then
+		animTrack:Play()
+	else
+		warn("Animation track does not exist!")
+		return
+	end
+end
+
+workspace.Mobs.ChildAdded:Connect(function(object)
+	playAnimation(object, "Walk")
+end)
+
+workspace.Towers.ChildAdded:Connect(function(object)
+	playAnimation(object, "Idle")
+end)
