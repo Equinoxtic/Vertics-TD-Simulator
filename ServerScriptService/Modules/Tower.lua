edada9a44c70570d288056e48b2fca971ab4c3c1
Vertic <thisistherealequinoctic@gmail.com>
2022-06-02 11:31:30 +0800

    Create a freshly improved tower module

--- ServerScriptService/Modules/Tower.lua
+++ ServerScriptService/Modules/Tower.lua
@@ -13,9 +13,41 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 -- Objects - Add in some variables here
 
 local spawnTowerEvent = ReplicatedStorage:WaitForChild("TowerSpawnEvent")
+local towerAnimateEvent = ReplicatedStorage:WaitForChild("TowerAnimateEvent")
+
+
+-- Functions - Code in some functions/local functions here
+
+local function FindNearestTarget(tower)
+	local maxDistance = 5
+	local nearestTarget = nil
+	for i, target in ipairs(workspace.Mobs:GetChildren()) do
+		local distance = (target.HumanoidRootPart.Position - tower.HumanoidRootPart.Position).Magnitude
+		print(target.Name, distance)
+		if distance < maxDistance then
+			print(target.Name, " is the nearest target")
+			nearestTarget = target
+			maxDistance = distance
+		end
+	end
+	return nearestTarget
+end
 
 
 -- Module Functions - Implement some functions for the module
+
+function tower.Attack(tower)
+	while true do
+		local target = FindNearestTarget(tower)
+		if target and target:FindFirstChild("Humanoid") and target.Humanoid.Health > 0 then
+			local targetCFrame = CFrame.lookAt(tower.HumanoidRootPart.Position, target.HumanoidRootPart.Position)
+			tower.HumanoidRootPart.BodyGyro.CFrame = targetCFrame
+			towerAnimateEvent:FireAllClients(tower, "Attack")
+			target.Humanoid:TakeDamage(2)
+		end
+		task.wait(1)
+	end
+end
 
 function tower.Spawn(player, name, pos)
 	local towerExists = ReplicatedStorage.Towers:FindFirstChild(name)
@@ -24,10 +56,17 @@ function tower.Spawn(player, name, pos)
 		towerToSpawn.HumanoidRootPart.CFrame = pos
 		towerToSpawn.Parent = workspace.Towers
 		towerToSpawn.HumanoidRootPart:SetNetworkOwner(nil)
+		local bodyGyro = Instance.new("BodyGyro")
+		bodyGyro.MaxTorque = Vector3.new(math.huge, math.huge, math.huge)
+		bodyGyro.D = 0
+		bodyGyro.CFrame = towerToSpawn.HumanoidRootPart.CFrame
+		bodyGyro.Parent = towerToSpawn.HumanoidRootPart
+		coroutine.wrap(tower.Attack)(towerToSpawn)
 	else
 		warn("Requested Tower does not exist: ", name)
 	end
 end
+
 
 -- Service(s) functions - Code in the functions that the services use
 
