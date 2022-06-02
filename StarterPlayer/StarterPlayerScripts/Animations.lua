21f3fb1cfe5fdc554d4665184c5feb0bcb0632e0
Vertic <thisistherealequinoctic@gmail.com>
2022-06-02 14:00:13 +0800

    Update Animation Handler (Animations.lua)

--- StarterPlayer/StarterPlayerScripts/Animations.lua
+++ StarterPlayer/StarterPlayerScripts/Animations.lua
@@ -7,6 +7,12 @@ local function setAnimation(object, animName)
 		local animObj = object:WaitForChild(animName)
 		if animObj then
 			local animator = humanoid:FindFirstChild("Animator") or Instance.new("Animator", humanoid)
+			local playingTracks = animator:GetPlayingAnimationTracks()
+			for i, track in pairs(playingTracks) do
+				if track.Name == animName then
+					return track
+				end
+			end
 			local animTrack = animator:LoadAnimation(animObj)
 			return animTrack
 		end
