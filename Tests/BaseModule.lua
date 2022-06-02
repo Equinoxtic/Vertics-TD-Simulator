f98249db1da2af9a3d22da2e5e9174c83146edfe
Vertic <thisistherealequinoctic@gmail.com>
2022-06-02 15:01:17 +0800

    Update BaseModule.lua

--- Tests/BaseModule.lua
+++ Tests/BaseModule.lua
@@ -2,6 +2,18 @@
 
 local base = {}
 
+-- Services - Implement some of the game's available services here
+
+local serverStorage = game:GetService("ServerStorage")
+
+
+-- Objects - Add in some variables here
+
+local bindables = serverStorage:WaitForChild("Bindables")
+local updateBaseHealthEvent = bindables:WaitForChild("UpdateBaseHealth")
+local gameOverEvent = bindables:WaitForChild("GameOver")
+
+
 -- Module Functions - Implement some functions for the module
 
 function base.Setup(map, health)
@@ -17,8 +29,17 @@ function base.UpdateHealth(damage)
 	local gui = base.Model.HealthGui
 	local percent = base.CurrentHealth / base.MaxHealth
 	gui.CurrentHealth.Size = UDim2.new(percent, 0, 0.5, 0)
-	gui.Title.Text = base.CurrentHealth .. "/" .. base.MaxHealth
+	if base.CurrentHealth <= 0 then
+		gameOverEvent:Fire()
+		gui.Title.Text = "Game Over!"
+	else
+		gui.Title.Text = base.CurrentHealth .. "/" .. base.MaxHealth
+	end
 end
+
+-- Service(s) functions - Code in the functions that the services use
+
+updateBaseHealthEvent.Event:Connect(base.UpdateHealth)
 
 
 -- Return the module (DO NOT REMOVE THIS AS WELL)
