114d67279efb2a1ac8b3de621ac8d599c88f1646
Vertic <thisistherealequinoctic@gmail.com>
2022-06-08 04:03:26 +0800

    Create SprintScript.client.lua

---
+++ src/StarterPlayer/StarterCharacterScripts/SprintScript.client.lua
@@ -0,0 +1,25 @@
+-- Services
+local inputService = game:GetService("UserInputService")
+local playerService = game:GetService("Players")
+
+-- Objects
+local plr = playerService.LocalPlayer
+local char = plr.Character or plr.CharacterAdded:wait()
+
+inputService.InputBegan:Connect(function (key)
+	if key.KeyCode == Enum.KeyCode.LeftShift or key.KeyCode == Enum.KeyCode.RightShift then
+		running = true
+		if char:FindFirstChild("Humanoid") then	
+			char.Humanoid.WalkSpeed = 18
+		end
+	end
+end)
+
+inputService.InputEnded:Connect(function (key)
+	if key.KeyCode == Enum.KeyCode.LeftShift or key.KeyCode == Enum.KeyCode.RightShift then
+		running = false
+		if char:FindFirstChild("Humanoid") then	
+			char.Humanoid.WalkSpeed = 24
+		end
+	end
+end)
