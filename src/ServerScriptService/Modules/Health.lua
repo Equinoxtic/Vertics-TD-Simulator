e9e9dd57bd7e29cac1c1436e8ad5f7c241f7e1d9
Vertic <thisistherealequinoctic@gmail.com>
2022-06-09 02:17:43 +0800

    Update Health.lua

--- src/ServerScriptService/Modules/Health.lua
+++ src/ServerScriptService/Modules/Health.lua
@@ -13,6 +13,13 @@ function health.Setup(model)
 	local newHealthBar = script.HealthGui:Clone()
 	newHealthBar.Adornee = model:WaitForChild("Head")
 	newHealthBar.Parent = Players.LocalPlayer.PlayerGui
+	if model.Name == "Base" then
+		newHealthBar.MaxDistance = 100
+		newHealthBar.Size = UDim2.new(0, 200, 0, 20)
+	else
+		newHealthBar.MaxDistance = 30
+		newHealthBar.Size = UDim2.new(0, 100, 0, 20)
+	end
 	health.UpdateHealth(newHealthBar, model)
 	model.Humanoid.HealthChanged:Connect(function()
 		health.UpdateHealth(newHealthBar, model)
@@ -23,7 +30,7 @@ function health.UpdateHealth(gui, model)
 	local humanoid = model:WaitForChild("Humanoid")
 	if humanoid and gui then
 		local percent = humanoid.Health / humanoid.MaxHealth
-		gui.CurrentHealth.Size = UDim2.new(percent, 0, 0.5, 0)
+		gui.CurrentHealth.Size = UDim2.new(percent, 0, 1, 0)
 		gui.Title.Text = humanoid.Health .. "/" .. humanoid.MaxHealth
 	end
 end
